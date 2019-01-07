d8e0eca8f7890b6ae51845b34d98ba983644de6a
SCHAEFAX <axel.schaeffer@externe.e-i.com>
2019-01-07 16:40:27 +0100

    Basic features implemented

--- TinyVideoPlayer/MainWindow.xaml.cs
+++ TinyVideoPlayer/MainWindow.xaml.cs
@@ -3,8 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using Vlc.DotNet.Core;
@@ -69,6 +67,11 @@ namespace TinyVideoPlayer
         /// </summary>
         public Uri CurrentFile { get; set; }
 
+        /// <summary>
+        /// Window start position.
+        /// </summary>
+        private Point WindowStartPos { get; set; }
+
         #endregion //Properties
 
         //#region NotifyPropertyChanged
@@ -97,6 +100,7 @@ namespace TinyVideoPlayer
             InitializeComponent();
 
             IsRepeating = true;
+            OriginMouseSpeed = (uint)System.Windows.Forms.SystemInformation.MouseSpeed;
 
             var vlcLibDirectory = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
             var options = new []
@@ -104,52 +108,56 @@ namespace TinyVideoPlayer
                 /*https://wiki.videolan.org/Documentation:Command_line/ */
                 "--file-logging", "-vvv", "--extraintf=logger", "--logfile=Logs.log" 
             };
+
             VideoControl.SourceProvider.CreatePlayer(vlcLibDirectory, options);
 
-            VideoControl.SetBinding(Canvas.TopProperty, new MultiBinding
-            {
-                Converter = new CenterConverter(),
-                ConverterParameter = "top",
-                Mode = BindingMode.TwoWay,
-                Bindings = {
-                    new Binding("ActualWidth") { Source = DropZone },
-                    new Binding("ActualHeight") { Source = DropZone },
-                    new Binding("ActualWidth") { Source = VideoControl },
-                    new Binding("ActualHeight") { Source = VideoControl }
-                }
-            });
-            VideoControl.SetBinding(Canvas.LeftProperty, new MultiBinding
-            {
-                Converter = new CenterConverter(),
-                ConverterParameter = "left",
-                Mode = BindingMode.TwoWay,
-                Bindings = {
-                    new Binding("ActualWidth") { Source = DropZone },
-                    new Binding("ActualHeight") { Source = DropZone },
-                    new Binding("ActualWidth") { Source = VideoControl },
-                    new Binding("ActualHeight") { Source = VideoControl }
-                }
-            });
-            VideoControl.RenderTransform = new TransformGroup
-            {
-                Children = new TransformCollection
-                {
-                    new TranslateTransform(),
-                    new ScaleTransform()
-                }
-            };
+            #region Needed?
+            //VideoControl.SetBinding(Canvas.TopProperty, new MultiBinding
+            //{
+            //    Converter = new CenterConverter(),
+            //    ConverterParameter = "top",
+            //    Mode = BindingMode.TwoWay,
+            //    Bindings = {
+            //        new Binding("ActualWidth") { Source = DropZone },
+            //        new Binding("ActualHeight") { Source = DropZone },
+            //        new Binding("ActualWidth") { Source = VideoControl },
+            //        new Binding("ActualHeight") { Source = VideoControl }
+            //    }
+            //});
+            //VideoControl.SetBinding(Canvas.LeftProperty, new MultiBinding
+            //{
+            //    Converter = new CenterConverter(),
+            //    ConverterParameter = "left",
+            //    Mode = BindingMode.TwoWay,
+            //    Bindings = {
+            //        new Binding("ActualWidth") { Source = DropZone },
+            //        new Binding("ActualHeight") { Source = DropZone },
+            //        new Binding("ActualWidth") { Source = VideoControl },
+            //        new Binding("ActualHeight") { Source = VideoControl }
+            //    }
+            //});
+            #endregion Needed?
 
+            VideoControl.RenderTransform = new TransformGroup { Children = new TransformCollection { new TranslateTransform(), new ScaleTransform() } };
             VideoControl.SourceProvider.MediaPlayer.EndReached += MediaPlayer_EndReached;
             DropZone.MouseWheel += DropZone_MouseWheel;
             DropZone.Drop += DropZone_Drop;
             DropZone.SizeChanged += DropZone_SizeChanged;
             DropZone.MouseLeftButtonDown += DropZone_MouseLeftButtonDown;
-            VideoControl.MouseMove += VideoControl_MouseMove;
-            VideoControl.MouseLeftButtonUp += DropZone_MouseLeftButtonUp;
+            DropZone.MouseMove += DropZone_MouseMove;
+            DropZone.MouseLeftButtonUp += DropZone_MouseLeftButtonUp;
+            DropZone.MouseDown += DropZone_MouseDown;
+            this.PreviewMouseRightButtonDown += MainWindow_PreviewMouseRightButtonDown;
+            this.PreviewMouseMove += MainWindow_PreviewMouseMove;
 
-            //CurrentFile = new Uri(@"C:\Users\axels\Downloads\Nouveau dossier\_.webm");
+            #region Testing
+
+            CurrentFile = new Uri(@"C:\Users\SCHAEFAX\Documents\Perso\testVid.webm");
             //CurrentFile = new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov");
-            //VideoControl.SourceProvider.MediaPlayer.Play(CurrentFile);
+            VideoControl.SourceProvider.MediaPlayer.Play(CurrentFile);
+            VideoControl.SourceProvider.MediaPlayer.Audio.IsMute = true;
+
+            #endregion //Testing
         }
 
         /// <summary>
@@ -158,6 +166,28 @@ namespace TinyVideoPlayer
         delegate void VlcRepeatDelegate(Uri fileUri, string[] pars);
 
         #region Events
+
+        /// <summary>
+        /// Handles window dragging.
+        /// </summary>
+        private void MainWindow_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.RightButton != MouseButtonState.Pressed) return;
+
+            var vector = e.GetPosition(this) - WindowStartPos;
+            Left += vector.X;
+            Top += vector.Y;
+        }
+
+        /// <summary>
+        /// Handles drag start.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowStartPos = e.GetPosition(this);
+        }
 
         /// <summary>
         /// Repeat feature implementation.
@@ -227,6 +257,27 @@ namespace TinyVideoPlayer
         }
 
         /// <summary>
+        /// Handles video toggle pause && kill app.
+        /// </summary>
+        private void DropZone_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount != 2) return;
+            
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (VideoControl.SourceProvider.MediaPlayer.IsPlaying())
+                    VideoControl.SourceProvider.MediaPlayer.Pause();
+                else
+                    VideoControl.SourceProvider.MediaPlayer.Play();
+            }
+            else if (e.ChangedButton == MouseButton.Right)
+            {
+                VideoControl.SourceProvider.Dispose();
+                Application.Current.Shutdown();
+            }
+        }
+
+        /// <summary>
         /// Zoom feature implementation.
         /// </summary>
         private void DropZone_MouseWheel(object sender, MouseWheelEventArgs args)
@@ -235,6 +286,17 @@ namespace TinyVideoPlayer
             var zoom = args.Delta > 0 ? ZoomRatio : -ZoomRatio;
             var relativePointCache = VideoControl.TranslatePoint(new Point(0, 0), DropZone);
 
+            var translateTransform = (TranslateTransform)((TransformGroup)VideoControl.RenderTransform).Children.First(tr => tr is TranslateTransform);
+            var height = VideoControl.ActualHeight * scaleTransform.ScaleY;
+            var width = VideoControl.ActualWidth * scaleTransform.ScaleX;
+
+            if (width + zoom < this.ActualWidth && height + zoom < this.ActualHeight && zoom < 0)
+            {
+                translateTransform.X = 0;
+                translateTransform.Y = 0;
+                return;
+            }
+
             if (scaleTransform.ScaleX + zoom > ZoomMinTreshold && scaleTransform.ScaleX + zoom < ZoomMaxTreshold &&
                 scaleTransform.ScaleY + zoom > ZoomMinTreshold && scaleTransform.ScaleY + zoom < ZoomMaxTreshold)
             {
@@ -242,13 +304,11 @@ namespace TinyVideoPlayer
                 scaleTransform.ScaleY += zoom;
             }
 
-            if (zoom > 0)
-                return;
+            if (zoom > 0) return;
 
-            var height = VideoControl.ActualHeight * scaleTransform.ScaleY;
-            var width = VideoControl.ActualWidth * scaleTransform.ScaleX;
-            var translateTransform = (TranslateTransform)((TransformGroup)VideoControl.RenderTransform).Children.First(tr => tr is TranslateTransform);
-            var relativePoint = VideoControl.TranslatePoint(new Point(0, 0), DropZone);
+            height = VideoControl.ActualHeight * scaleTransform.ScaleY;
+            width = VideoControl.ActualWidth * scaleTransform.ScaleX;
+            var relativePoint = VideoControl.TransformToVisual(DropZone).Transform(new Point(0, 0));
             var mathRelativePositionY = Math.Round(relativePoint.Y - relativePointCache.Y, MidpointRounding.AwayFromZero);
             var mathRelativePositionX = Math.Round(relativePoint.X - relativePointCache.X, MidpointRounding.AwayFromZero);
             var mathTranslateY = Math.Round(translateTransform.Y, MidpointRounding.AwayFromZero);
@@ -285,43 +345,45 @@ namespace TinyVideoPlayer
         /// <summary>
         /// Move the media element with the mouse.
         /// </summary>
-        private void VideoControl_MouseMove(object sender, MouseEventArgs e)
+        private void DropZone_MouseMove(object sender, MouseEventArgs e)
         {
-            if (!VideoControl.IsMouseCaptured) return;
-
-            var scaleTransform = (ScaleTransform)((TransformGroup)VideoControl.RenderTransform).Children.First(tr => tr is ScaleTransform);
-            var controlHeight = VideoControl.ActualHeight * scaleTransform.ScaleY;
-            var controlWidth = VideoControl.ActualWidth * scaleTransform.ScaleX;
-
-            //TransformToAncestor
-            Point relativePoint = VideoControl.TransformToVisual(DropZone).Transform(new Point(0, 0));
-            var mathRelativeY = Math.Round(relativePoint.Y, MidpointRounding.AwayFromZero);
-            var mathRelativeX = Math.Round(relativePoint.X, MidpointRounding.AwayFromZero);
-            var mathMouseY = Math.Round(MousePosition.Y, MidpointRounding.AwayFromZero);
-            var mathMouseX = Math.Round(MousePosition.X, MidpointRounding.AwayFromZero);
-
-            Vector vector = new Vector { X = 0, Y = 0 };
-            if (controlHeight >= DropZone.ActualHeight)
+            if (DropZone.IsMouseCaptured)
             {
-                var vectorY = Math.Round(e.GetPosition(DropZone).Y, MidpointRounding.AwayFromZero) - mathMouseY;
-                if (mathRelativeY <= 0 && mathRelativeY + vectorY <= 0 && vectorY > 0 ||
-                    mathRelativeY + controlHeight >= DropZone.ActualHeight && mathRelativeY + controlHeight + vectorY >= DropZone.ActualHeight && vectorY < 0)
-                    vector.Y = vectorY;
+                var scaleTransform = (ScaleTransform)((TransformGroup)VideoControl.RenderTransform).Children.First(tr => tr is ScaleTransform);
+                var controlHeight = VideoControl.ActualHeight * scaleTransform.ScaleY;
+                var controlWidth = VideoControl.ActualWidth * scaleTransform.ScaleX;
+
+                Point relativePoint = VideoControl.TransformToVisual(DropZone).Transform(new Point(0, 0));
+                var mathRelativeY = Math.Round(relativePoint.Y, MidpointRounding.AwayFromZero);
+                var mathRelativeX = Math.Round(relativePoint.X, MidpointRounding.AwayFromZero);
+                var mathMouseY = Math.Round(MousePosition.Y, MidpointRounding.AwayFromZero);
+                var mathMouseX = Math.Round(MousePosition.X, MidpointRounding.AwayFromZero);
+
+                Vector vector = new Vector { X = 0, Y = 0 };
+                if (controlHeight >= DropZone.ActualHeight)
+                {
+                    var vectorY = Math.Round(e.GetPosition(DropZone).Y, MidpointRounding.AwayFromZero) - mathMouseY;
+                    if (mathRelativeY <= 0 && mathRelativeY + vectorY <= 0 && vectorY > 0 || mathRelativeY + controlHeight >= DropZone.ActualHeight && mathRelativeY + controlHeight + vectorY >= DropZone.ActualHeight && vectorY < 0)
+                        vector.Y = vectorY;
+                }
+                if (controlWidth >= DropZone.ActualWidth)
+                {
+                    var vectorX = Math.Round(e.GetPosition(DropZone).X, MidpointRounding.AwayFromZero) - mathMouseX;
+                    if (mathRelativeX <= 0 && mathRelativeX + vectorX <= 0 && vectorX > 0 || mathRelativeX + controlWidth >= DropZone.ActualWidth && mathRelativeX + controlWidth + vectorX >= DropZone.ActualWidth && vectorX < 0)
+                        vector.X = vectorX;
+                }
+
+                var translateTransform = (TranslateTransform)((TransformGroup)VideoControl.RenderTransform).Children.First(tr => tr is TranslateTransform);
+                translateTransform.X = OriginPosition.X + vector.X;
+                translateTransform.Y = OriginPosition.Y + vector.Y;
+
+                MousePosition = e.GetPosition(DropZone);
+                OriginPosition = new Point(Math.Round(translateTransform.X, MidpointRounding.AwayFromZero), Math.Round(translateTransform.Y, MidpointRounding.AwayFromZero));
             }
-            if (controlWidth >= DropZone.ActualWidth)
+            else
             {
-                var vectorX = Math.Round(e.GetPosition(DropZone).X, MidpointRounding.AwayFromZero) - mathMouseX;
-                if (mathRelativeX <= 0 && mathRelativeX + vectorX <= 0 && vectorX > 0 ||
-                    mathRelativeX + controlWidth >= DropZone.ActualWidth && mathRelativeX + controlWidth + vectorX >= DropZone.ActualWidth && vectorX < 0)
-                    vector.X = vectorX;
+                //TODO Slow down/up effect
             }
-
-            var translateTransform = (TranslateTransform)((TransformGroup)VideoControl.RenderTransform).Children.First(tr => tr is TranslateTransform);
-            translateTransform.X = OriginPosition.X + vector.X;
-            translateTransform.Y = OriginPosition.Y + vector.Y;
-
-            MousePosition = e.GetPosition(DropZone);
-            OriginPosition = new Point(Math.Round(translateTransform.X, MidpointRounding.AwayFromZero), Math.Round(translateTransform.Y, MidpointRounding.AwayFromZero));
         }
 
         /// <summary>
@@ -329,7 +391,7 @@ namespace TinyVideoPlayer
         /// </summary>
         private void DropZone_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            VideoControl.ReleaseMouseCapture();
+            DropZone.ReleaseMouseCapture();
             VideoControl.Cursor = Cursors.Arrow;
             SystemParametersInfo(0x0071, 0, OriginMouseSpeed, 0);
         }
@@ -346,13 +408,12 @@ namespace TinyVideoPlayer
             if (elementWidth <= DropZone.ActualWidth && elementHeight <= DropZone.ActualHeight) return;
 
             VideoControl.Cursor = Cursors.SizeAll;
-            VideoControl.CaptureMouse();
+            DropZone.CaptureMouse();
 
             var translateTransform = (TranslateTransform)((TransformGroup)VideoControl.RenderTransform).Children.First(tr => tr is TranslateTransform);
             MousePosition = e.GetPosition(DropZone);
             OriginPosition = new Point(Math.Round(translateTransform.X, MidpointRounding.AwayFromZero), Math.Round(translateTransform.Y, MidpointRounding.AwayFromZero));
 
-            OriginMouseSpeed = (uint)System.Windows.Forms.SystemInformation.MouseSpeed;
             SystemParametersInfo(0x0071, 0, 3, 0);
         }
 
